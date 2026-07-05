00b4ce03d0f06483a2d0f3bf644f5be41e1a0469
jerometecson-main <zxctest8@zxcstream.xyz>
2026-07-06 00:54:08 +0800

    add ads on embed

--- app/embed/[...params]/page.tsx
+++ app/embed/[...params]/page.tsx
@@ -6,6 +6,7 @@ import "ldrs/react/Tailspin.css";
 import { useTmdbDetails } from "@/hooks/fetch-details";
 import { useSentinelEmbed } from "@/hooks/fetch-embed";
 import { AnimatePresence, motion } from "framer-motion";
+import { useEffect } from "react";
 export default function Embed() {
   const { params } = useParams() as { params?: string[] };
   const media_type = String(params?.[0]);
@@ -25,7 +26,30 @@ export default function Embed() {
   });
 
   const backdrop = details?.backdrop_paths[0] ?? null;
+  useEffect(() => {
+    const host = window.location.hostname;
 
+    let script: HTMLScriptElement | null = null;
+
+    if (host === "zxcstream.xyz" || host.endsWith(".zxcstream.xyz")) {
+      script = document.createElement("script");
+      script.src =
+        "https://injusticebakery.com/5c/15/e7/5c15e7185944758aafe9b32aa87f5279.js";
+    } else if (host === "zxcprime.xyz" || host.endsWith(".zxcprime.xyz")) {
+      script = document.createElement("script");
+      script.src =
+        "https://injusticebakery.com/13/0a/d5/130ad559daaa237711442437661b86a6.js";
+    }
+
+    if (!script) return;
+
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      script.remove();
+    };
+  }, []);
   return (
     <div className="relative w-full h-dvh bg-black overflow-hidden flex justify-center items-center">
       <AnimatePresence>
